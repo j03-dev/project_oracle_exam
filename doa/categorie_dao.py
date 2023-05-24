648cc4e0b123d0cf79b7e16d72981efcaa880be5
j03-dev <24nomeniavo@gmail.com>
2023-05-24 22:58:54 +0300

    update: finish to implement categorie_dao.py without test

--- doa/categorie_dao.py
+++ doa/categorie_dao.py
@@ -1,22 +1,68 @@
 from doa import Dao
+from entity import Categorie
+from setting import database_connection
 
 
 class CategorieDao(Dao):
-    def create(self, T) -> bool:
-        sql = "insert into categorie(name) values (?)"
-        self.conn.cursor().execute(sql)
+    def create(self, entity: Categorie) -> bool:
+        try:
+            sql = "insert into categorie(name, id_admin) values (:name, :id_admin)"
+            self.conn.cursor().execute(sql, name=entity.name, id_admin=entity.id_admin)
+            self.conn.commit()
+            return True
+        except Exception as e:
+            print(f"Error {e}")
+            return False
 
-    def update(self, _id: int) -> bool:
-        sql = "update categorie set name=? where id=?"
-        pass
+    def update(self, entity: Categorie) -> bool:
+        try:
+            sql = "update categorie set name=:name where id=:id"
+            self.conn.cursor().execute(sql, name=entity.name, id=entity.id)
+            self.conn.commit()
+            return True
+        except Exception as e:
+            print(f"Error {e}")
+            return False
 
     def delete(self, _id: int) -> bool:
-        sql = "delete from categorie where id=?"
-        pass
+        try:
+            sql = "delete from categorie where id=:id"
+            self.conn.cursor().execute(sql, id=_id)
+            self.conn.commit()
+            return True
+        except Exception as e:
+            print("error {}", e)
+            return False
 
-    def get_by_id(self, _id: int) -> object:
-        sql = "select * from categorie where id=?"
+    def get_by_id(self, _id: int) -> Categorie:
+        sql = "select * from categorie where id=:id"
+        cursor = self.conn.cursor()
+        cursor.execute(sql, id=_id)
+        result = cursor.fetchone()
+        if result is None:
+            return Categorie()
+        id_, name, id_admin = result
+        cursor.close()
+        return Categorie(id_, name, id_admin)
 
-    def get_all(self) -> object:
+    def get_all(self) -> list[Categorie]:
         sql = "select * from categorie"
-        pass
+        cursor = self.conn.cursor()
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        if len(result) == 0:
+            return result
+        cursor.close()
+        return [Categorie(id_, name, id_admin) for id_, name, id_admin in result]
+
+
+if __name__ == "__main__":
+    conn = database_connection()
+    categorie_dao = CategorieDao(conn)
+    # categorie = Categorie(name="Élevage", id_admin=1)
+    # print(categorie_dao.create(categorie))
+
+    categorie = categorie_dao.get_all()
+    print(categorie[0].name)
+
+    print(categorie_dao.get_by_id(2))
