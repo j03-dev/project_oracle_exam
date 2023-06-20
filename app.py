5ecfe45f3a4d123e39f6d823ea292b2bfeb481cc
j03-dev <24nomeniavo@gmail.com>
2023-06-20 07:57:39 +0300

    chande the directory of admin blueprint

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-from admin.app import admin, produit_dao, categorie_dao
+from admin import admin, produit_dao, categorie_dao
 
 app = Flask(__name__)
 app.secret_key = "24C5Bda7cB3150E20be510065B7cff9ea9caB7DA"
@@ -25,7 +25,6 @@ def search():
     produits = produit_dao.get_all()
     if request.method == "POST":
         name = request.form.get('name')
-        print(name)
         s_produits = [prod for prod in produits if name.lower() in prod.name.lower()]
         return render_template("index.html", produits=s_produits)
 
@@ -37,4 +36,4 @@ def detail(id_: int):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
